c44e35be8803b02a3a22762beebc74a35ec8158c
Utku <93601245+inTheOctagon@users.noreply.github.com>
2023-09-11 03:01:38 +0300

    Add files via upload

--- UniversityQuery.sql
+++ UniversityQuery.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE University
-GO
+--CREATE DATABASE University
+--GO
 USE University
 GO
 CREATE TABLE Terms(
@@ -71,12 +71,11 @@ CREATE TABLE Courses(
 						CONSTRAINT PK_Courses PRIMARY KEY(CourseID)
 					)
 GO
-CREATE TABLE StudentAcademicCourses(
+CREATE TABLE StudentCourses(
 									CourseID int,
 									StudentID int,
 									TermID int,
 									CourseYear int,
-									AcademicID int,
 									Midterm1Percent int CHECK(Midterm1Percent>=0 AND 100>=Midterm1Percent),
 									Midterm2Percent int CHECK(Midterm2Percent>=0 AND 100>=Midterm2Percent),
 									Midterm3Percent int CHECK(Midterm3Percent>=0 AND 100>=Midterm3Percent),
@@ -85,17 +84,46 @@ CREATE TABLE StudentAcademicCourses(
 									AveragePercent int CHECK(AveragePercent>=0 AND 100>=AveragePercent),
 									LetterGrade nvarchar(2),
 									CONSTRAINT PK_StudentAcademicCourses PRIMARY KEY(CourseID, StudentID, TermID, CourseYear),
-									CONSTRAINT FK_SAC_AcademicID FOREIGN KEY(AcademicID) REFERENCES Academics(AcademicID)
+									CONSTRAINT FK_SC_CourseID FOREIGN KEY(CourseID) REFERENCES Courses(CourseID)
 									ON UPDATE CASCADE,
-									CONSTRAINT FK_SAC_CourseID FOREIGN KEY(CourseID) REFERENCES Courses(CourseID)
+									CONSTRAINT FK_SC_StudentID FOREIGN KEY(StudentID) REFERENCES Students(StudentID)
 									ON UPDATE CASCADE,
-									CONSTRAINT FK_SAC_StudentID FOREIGN KEY(StudentID) REFERENCES Students(StudentID)
+									CONSTRAINT FK_SC_TermID FOREIGN KEY(TermID) REFERENCES Terms(TermID)
 									ON UPDATE CASCADE,
-									CONSTRAINT FK_SAC_TermID FOREIGN KEY(TermID) REFERENCES Terms(TermID)
-									ON UPDATE CASCADE,
-									CONSTRAINT FK_SAC_CourseYear FOREIGN KEY(CourseYear) REFERENCES Years([Year])
+									CONSTRAINT FK_SC_CourseYear FOREIGN KEY(CourseYear) REFERENCES Years([Year])
 									ON UPDATE CASCADE
 					)
+GO
+CREATE TABLE LecturerCourses(
+								CourseID int,
+								TermID int,
+								CourseYear int,
+								AcademicID int,
+								CONSTRAINT PK_LecturerCourses PRIMARY KEY(CourseID, TermID, CourseYear),
+								CONSTRAINT FK_LC_CourseID FOREIGN KEY(CourseID) REFERENCES Courses(CourseID)
+								ON UPDATE CASCADE,
+								CONSTRAINT FK_LC_AcademicID FOREIGN KEY(AcademicID) REFERENCES Academics(AcademicID)
+								ON UPDATE CASCADE,
+								CONSTRAINT FK_LC_TermID FOREIGN KEY(TermID) REFERENCES Terms(TermID)
+								ON UPDATE CASCADE,
+								CONSTRAINT FK_LC_CourseYear FOREIGN KEY(CourseYear) REFERENCES Years([Year])
+								ON UPDATE CASCADE
+                            )
+GO
+CREATE TABLE StudentTermGPA(
+							StudentID int,
+							TermID int,
+							TermYear int,
+							GPA decimal(2,1)
+							CONSTRAINT PK_STGPA PRIMARY KEY(StudentID, TermID, TermYear),
+							CONSTRAINT FK_STGPA_StudentID FOREIGN KEY(StudentID) REFERENCES Students(StudentID)
+							ON UPDATE CASCADE,
+							CONSTRAINT FK_STGPA_TermID FOREIGN KEY(TermID) REFERENCES Terms(TermID)
+							ON UPDATE CASCADE,
+							CONSTRAINT FK_STGPA_CourseYear FOREIGN KEY(TermYear) REFERENCES Years([Year])
+							ON UPDATE CASCADE
+)
+
 GO
 CREATE TABLE Articles(
 						ArticleID int,
@@ -113,3 +141,20 @@ CREATE TABLE AuthorArticles(
 						CONSTRAINT FK_AA_ArticleID FOREIGN KEY(ArticleID) REFERENCES Articles(ArticleID)
 						ON UPDATE CASCADE
 						   )
+GO
+
+
+		-- Articles tablosu ekle: PublicationYear, ArticleName, AcademicID, DepartmentID
+--DROP TABLE Academics
+--DROP TABLE Courses
+--DROP TABLE Departments
+--DROP TABLE Faculties
+--DROP TABLE Students
+--DROP TABLE Terms
+--DROP TABLE Years
+--DROP TABLE RegistrationDetails
+--DROP TABLE Articles
+--DROP TABLE AuthorArticles
+--DROP TABLE StudentTermGPA
+--DROP TABLE StudentCourses
+--DROP TABLE LecturerCourses
